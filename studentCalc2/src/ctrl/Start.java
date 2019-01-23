1611cc952d81f34cf0dbf81dc6114284dce8222d
Amanda D'Errico <aderrico@my.yorku.ca>
2019-01-22 23:16:33 -0500

    Finished calculation grace Interest and new Osap Payment. Restart button working.

--- studentCalc2/src/ctrl/Start.java
+++ studentCalc2/src/ctrl/Start.java
@@ -40,48 +40,8 @@ public class Start extends HttpServlet {
 		//This is a test
 		/**
 		 * Writing to the servlet to reply to the client with a message 
-		 */
-//		response.setContentType("text/plain");
-//		Writer resOut = response.getWriter();
-		
-//		// Part B
-//		resOut.write("\nHello, World!\n");
-//		
-//		
-//		String clientIP = request.getRemoteAddr();
-//		resOut.write("Client IP: " + clientIP + "\n");
-//		int portNum = request.getRemotePort();
-//		resOut.write("Port Num: " + portNum + "\n");
-//		String protocol = request.getProtocol();
-//		resOut.write("Protocol: " + protocol + "\n");
-//		String method = request.getMethod();
-//		resOut.write("Method: " + method + "\n");
-//		
-//		String clientQueryString = request.getQueryString();
-//		String p = request.getParameter("protocol");
-//		resOut.write("Query Param Protocol = " + p + "\n");
-//		String pn = request.getParameter("portNum");
-//		resOut.write("Query Param PortNum = " + pn + "\n");
-//		String m = request.getParameter("method");
-//		resOut.write("Query Param Method = " + m + "\n");
-		
-		String url = this.getServletContext().getContextPath() + "/Start";
-		StringBuffer URL = request.getRequestURL();
-		String URI = request.getRequestURI();
-		String context = request.getServletContext().getContextPath();
-		String servlet = request.getServletPath();
-		
-//		resOut.write("URL: " + URL + "\n");
-//		resOut.write("URI: " + URI + "\n");
-//		resOut.write("Context: " + context + "\n");
-//		resOut.write("Servlet: " + servlet + "\n");		
+		 */	
 
-		// get end of string to redirect back to Start 
-		// StartUp/YorkBank
-		String StartUpYorkBank = "/Startup/YorkBank";
-		if (URI.endsWith(StartUpYorkBank)) {
-			response.sendRedirect(url);
-		}
 		
 		
 		// Part C to print principal
@@ -92,9 +52,13 @@ public class Start extends HttpServlet {
 		// Part C - generate Exception and 404 error
 		// For exception, try to parse a double as a string: Double except = Double.parseDouble("amanda");
 		// For 404 error, type garbage: http://localhost:8080/studentCalc1/Startjhmbjhb
+
+		
 		
 		String sub = request.getParameter("submit");
 		if (sub != null) {
+			String targetRes = "/Results.jspx";
+			
 			String A = request.getParameter("principal"); 
 			String n = request.getParameter("period");
 			String r = request.getParameter("interest");
@@ -105,51 +69,60 @@ public class Start extends HttpServlet {
 			Double totalInterest = (double) 0;
 			
 			Double principal = Double.parseDouble(this.getServletContext().getInitParameter("principal"));
+			System.out.println("Value of prin = " + principal);
 			Double period = Double.parseDouble(this.getServletContext().getInitParameter("period"));
 			Double interest = Double.parseDouble(this.getServletContext().getInitParameter("interest"));
 			Double gracePeriod = Double.parseDouble(this.getServletContext().getInitParameter("gracePeriod"));
 			Double fixedInterest = Double.parseDouble(this.getServletContext().getInitParameter("fixedInterest"));	
 			
-			if (A != null) {
+			if (A != "") {
 				principal = Double.parseDouble(A);
 			}
-			
+		//	System.out.println("Value of prin = " + principal);
 			if (n != null) {
 				period = Double.parseDouble(n);			
 			}
-			
-			if (r != null) {
+		//	System.out.println("Value of period = " + period);
+			if (r != "") {
 				interest = Double.parseDouble(r);
 				//totalInterest = interest;
 			}
-			
+		//	System.out.println("Value of interest = " + interest);
 			// if grace period changes in the govt (i.e its 0 now)
 			if (gPeriod != null) {
 				gracePeriod = Double.parseDouble(gPeriod);			
 			}
-			
+		//	System.out.println("Value of grace period = " + gracePeriod);	
 			if (fixedR != null) {
 				fixedInterest = Double.parseDouble(fixedR);		
 			}
+			System.out.println("Value of fixed interest = " + fixedInterest);
 			// new total interest
 			totalInterest = fixedInterest + interest;
 	
 			Double osapFormula = ((interest/12)*principal)/(1 - Math.pow(1 + interest/12, -period));
 			Double graceInterest = (principal*(((totalInterest)/12)*gracePeriod));
 			Double osapWithGrace = osapFormula + (graceInterest / gracePeriod);
+			
 			DecimalFormat df = new DecimalFormat("#.##");
-			String roundedOsap = df.format(osapWithGrace); 
+			String roundedOsap = df.format(osapWithGrace);
+			DecimalFormat df2 = new DecimalFormat("#.##");
+			String roundedGrace = df2.format(graceInterest); 
+			
+			request.setAttribute("graceR",roundedGrace);
+			request.setAttribute("mPayment",roundedOsap);
 	
-			response.setContentType("text/plain");
-			Writer resOut = response.getWriter();
-			resOut.write("Total Monthly Payments with grace period and interest: " + roundedOsap + "\n");			
+
+			request.getRequestDispatcher(targetRes).forward(request, response);	
+			
+//			response.setContentType("text/plain");
+//			Writer resOut = response.getWriter();
+//			resOut.write("Total Monthly Payments with grace period and interest: " + roundedOsap + "\n");
 		}
 		else {
-			
+			String target = "/UI.jspx";
+			request.getRequestDispatcher(target).forward(request, response);		
 		}
-		
-		String target = "/UI.jspx";
-		request.getRequestDispatcher(target).forward(request, response);
 		
 	}
 	
